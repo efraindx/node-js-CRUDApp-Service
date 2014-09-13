6a8cfec03a2734e4caeb8c035a8e50393cf2a2cc
Efrain Toribio Reyes <efrainreyes05@hotmail.com>
2014-09-13 13:57:15 +0200

    allowing access control to service..

--- server.js
+++ server.js
@@ -117,7 +117,7 @@ app.use('/api', router);
 app.use(function (req, res, next) {
 
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8085');
 
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
